6cb3324f35a8eb1895ec31d9af52499e951599d8
Eric <koreric75@gmail.com>
2016-10-24 20:22:04 -0600

    Create pisink.sh

---
+++ pisink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# script to synchronise Pi files to backup
+BACKUP_MOUNTED=$(mount | awk '/pibackup/ {print $6}' | grep "rw")
+if [ $BACKUP_MOUNTED ]; then
+    echo $BACKUP_MOUNTED
+    echo "Commencing Backup"
+    rsync -avH --delete-during --delete-excluded --exclude-from=/usr/bin/rsync-exclude.txt / /mnt/pibackup/
+else
+    echo "Backup drive not available or not writable"
+fi
